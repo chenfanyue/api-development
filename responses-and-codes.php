cb9a7801717dcb9e21167278dc83877da87db21e
chenfanyue <nicegogogo@163.com>
2017-01-05 16:57:23 +0800

    Create responses-and-codes.php

---
+++ responses-and-codes.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Lesson;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+
+class LessonsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $lessons= Lesson::all();
+        return Response::json([
+            'data'=>$lessons->toArray()
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $lesson = Lesson::find($id);
+        if(! $lesson){
+            return Response::json([
+                'error'=>[
+                    'message'=>'lesson not found.',
+                    'code'=>900
+                ]
+            ],404);
+        }
+
+        return Response::json([
+            'data'=>$lesson
+        ],200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
+
+/*
+Two types of codes are mentioned here:
+1, HTTP response codes
+2, somehow 'customed' codes
+
+rfc-editor.org
+*/
